26bdd8bc6471c6a21e1282af779eae0a5707b8d0
Martins <ciekurs.martins@gmail.com>
2020-11-23 21:39:27 +0200

    Minor fix

--- functions.c
+++ functions.c
@@ -35,19 +35,25 @@ int get_named_argument(char* key, int argc, char **argv, char** result) {
       continue; // not found
     }
 
-    printf("HAPPY! %d, %s, j=%d\n", i, argv[i], j);
+    //printf("HAPPY! %d, %s, j=%d\n", i, argv[i], j);
 
     if (argv[i][j] != '=') {
       continue; // not found
     }
 
+
+    //printf("HAPPY2! %d, %s, j=%d\n", i, argv[i], j);
+
     if ((strlen(argv[i]) < strlen(key)+3) || strlen(argv[i]) - strlen(key) - 2 < 1) {
       return -3; // not found
     }
+    
+    
+    //printf("HAPPY3! %d, %s, j=%d\n", i, argv[i], j);
 
-    return 0;
+    //return 0;
 
-    printf("HAPPY 2! %d, %s\n", i, argv[i]);
+    //printf("HAPPY 2! %d, %s\n", i, argv[i]);
 
     // copy
     *result = (char*) malloc(sizeof(char)*(strlen(argv[i]) - strlen(key) - 1));
@@ -68,15 +74,20 @@ int get_port(char* key, int argc, char** argv) {
   char* port_c = NULL;
   int result = get_named_argument(key, argc, argv, &port_c);
   int i = 0;
+  
+  if (result < 0) {
+    return -1; // err
+  }
+  
   while(port_c[i]) {
+    /* printf("Currently checking %c\n", port_c[i]); */
     if (port_c[i] < 48 || port_c[i] > 57) {
+      /* printf("Not only nums in port provided\n"); */
       return -2; // err contains not numbers
     }
     i++;
   }
-  if (result < 0) {
-    return -1; // err
-  } else {
-    return atoi(port_c);
-  }
+
+  return atoi(port_c);
+
 }
